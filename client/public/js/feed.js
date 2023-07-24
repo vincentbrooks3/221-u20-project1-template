b15e11384432bc1cc7cfb759411fe0dac225e64c
Vincent Brooks <vb1311@students.necsd.net>
2023-07-24 14:43:54 -0400

    This is a commit message

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -3,31 +3,44 @@ document.getElementById('portal_button').addEventListener('click', (event) => {
 })
 
 let feedItem1 = {
-    title: 'title1',
-    body: 'body1',
-    linkUrl: 'linkUrl1',
-    imageUrl: 'imageUrl1',
+    title: 'Travis Scott - Rodeo',
+    body: 'Rodeo is the debut studio album by American rapper and record producer Travis Scott.',
+    linkUrl: 'https://open.spotify.com/album/4PWBTB6NYSKQwfo79I3prg?si=3e96b51LRii2Q2sn-Y-5kw',
+    imageUrl: '/images/Rodeoalbum.jpg',
 }
 
 let feedItem2 = {
-    title: 'title2',
-    body: 'body2',
-    linkUrl: 'linkUrl2',
-    imageUrl: 'imageUrl2',
+    title: 'A$AP Rocky - Live. Love. ASAP',
+    body: 'Live. Love. ASAP is the debut mixtape by American rapper ASAP Rocky.',
+    linkUrl: 'https://open.spotify.com/album/4l6EPpP9hjQrLb8qNB9eC5?si=G31OWaDnT62D7KrA5ptz3w',
+    imageUrl: '/images/Live_Love_ASAP.jpg',
 }
 
 let feedItem3 = {
-    title: 'title3',
-    body: 'body3',
-    linkUrl: 'linkUrl3',
-    imageUrl: 'imageUrl3',
+    title: 'Yeat - Lyfe',
+    body: 'Lyfe is the sixth extended play by American rapper Yeat.',
+    linkUrl: 'https://open.spotify.com/album/6Xo2PDEoQKzCndIbks2kvu?si=1Bzll-yASSuaMJI97sS7vw',
+    imageUrl: '/images/download(3).jpg',
 }
 
-var currentStories = (Story1, Story2, Story3);
-console.log(currentStories);
-
-function displayItem(currentStories) {
-    document.getElementById(newsfeed).innerHTML = currentStories;
-}
+var currentStories = (feedItem1, feedItem2, feedItem3);
 
 const currentStories = ["feedItem1", "feedItem2", "feedItem3"];
+
+console.log(currentStories);
+
+window.addEventListener("load", () => {
+    for(var i=0; i < currentStories.length; i++){
+        displayFeed(currentStories[i]);
+    }
+});
+
+function displayItem(currentStories) {
+   let feed = document.getElementById('newsfeed');
+   feed.innerHTML += "<spam><img class='myImage' src ='"+ currentStories.imageUrl +"'>" + "</spam>";
+   feed.innerHTML += "<spam><h2><a href= '" + currentStories.linkUrl + "'> " + currentStories.Title + "</a href>";
+   feed.innerHTML += "<p>" + currentStories.Body + "</p>";
+   feed.innerHTML += "<hr />";
+}
+
+
